8e1992cad6d329655ff34a4793438ba09c2bfe88
Luis <luishv94@gmail.com>
2014-05-30 10:36:31 -0500

    Versiones pruebas 1

---
+++ Proyect/DisplayAndButton/DisplayAndButton.ino
@@ -0,0 +1,67 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int buttonOnePin = 8;
+int counter=2;
+int buttonOneState;
+int lastButtonOneState = LOW;
+int ledStateOne = HIGH;
+long lastDebounceTimeOne = 0;
+long debounceDelay = 50;
+void setup() {
+  lcd.begin(16, 2);
+  
+  lcd.print("Cuantas veces?:");
+  
+  int readingOne = digitalRead(buttonOnePin);
+     if (readingOne != lastButtonOneState) {
+          lastDebounceTimeOne = millis();}
+       if ((millis() - lastDebounceTimeOne) > debounceDelay) {
+             if (readingOne != buttonOneState) {
+                         buttonOneState = readingOne;
+                       if (buttonOneState == HIGH) {
+                              ledStateOne = !ledStateOne;}}}
+ 
+    
+     if(ledStateOne!=lastButtonOneState){
+       lcd.clear(); 
+       
+     lcd.setCursor(1,2);
+       lcd.print("madre pendejo"); 
+       delay(8);
+     }
+     else if(ledStateOne!=1){
+       lcd.clear();
+    
+     lcd.setCursor(1,2);
+       lcd.print("madre pendejo");
+       delay(8);
+       
+     }
+     lastButtonOneState = readingOne;
+  
+  pinMode (8,INPUT);
+}
+void loop() {
+int readingOne = digitalRead(buttonOnePin);
+     if (readingOne != lastButtonOneState) {
+          lastDebounceTimeOne = millis();}
+       if ((millis() - lastDebounceTimeOne) > debounceDelay) {
+             if (readingOne != buttonOneState) {
+                       buttonOneState = readingOne;
+                       if (buttonOneState == HIGH) {
+                         if(ledStateOne==1&&counter<5){
+                           counter++;}
+                         if(counter==5){
+                           counter=2;}
+                         lcd.clear();
+                         lcd.print("Cuantas veces?");
+                         lcd.setCursor(1,2);
+                         lcd.print(counter);
+                         delay(100);}}}
+     lastButtonOneState = readingOne
+     
+     
+     
+     
+     
+}
---
+++ Proyect/StepperMotorPrueba/StepperMotorPrueba.ino
@@ -0,0 +1,30 @@
+int A=1;
+int B=2;
+int C=3;
+int D=4;
+void setup(){
+  pinMode(A,OUTPUT);
+  pinMode(B,OUTPUT);
+  pinMode(C,OUTPUT);
+  pinMode(D,OUTPUT);
+  digitalWrite(A,LOW);
+  digitalWrite(B,LOW);
+  digitalWrite(C,LOW);
+  digitalWrite(D,LOW);
+}
+void loop(){
+  digitalWrite(A,HIGH);
+  digitalWrite(B,HIGH);
+  delay(300);
+  digitalWrite(A,LOW);
+  digitalWrite(C,HIGH);
+  delay(300);
+  digitalWrite(B,LOW);
+  digitalWrite(D,HIGH);
+  delay(300);
+  digitalWrite(C,LOW);
+  digitalWrite(A,HIGH);
+  delay(300);
+  digitalWrite(D,LOW);
+  digitalWrite(A,LOW);
+}
